2ed26a4e10ef19cddda4c9eaef9dd096a6bb3bd9
snezhanakisel <snezhok-88@mail.ru>
2021-09-24 23:42:10 +0300

    create product date

--- myblog/main/urls.py
+++ myblog/main/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('blog/', blog),
     path('404', error404, name='404'),
     path('blog/<slug:post_slug>/', show_article),
-    path('registernewuser/', RegisterUser.as_view(), name='register'),
+    path('reg/', RegisterUser.as_view(), name='register'),
     path('login/', LoginUser.as_view()),
     path('accounts/profile/', profile)
 ]
